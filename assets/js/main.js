7c119d9667c9090e4f181155923c921db6779820
yoyoyo4649 <kd1351684@st.kobedenshi.ac.jp>
2023-07-10 12:27:38 +0900

    PC表示完成してます。

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,3 @@
-// $('.p-step__sliderList').slick({
-//   autoplay: true,
-//   dots: true,
-//   // スライドの最後まできたら次には進まない
-//   infinite: false,
-//   speed: 300,
-//   // 通常時はスライドは4枚表示、4枚ずつスライド
-//   slidesToShow: 4,
-//   slidesToScroll: 4,
-//   responsive: [
-//     {
-//       breakpoint: 1024,
-//       settings: {
-//         // 1024px以下の時、スライドは3枚表示、3枚ずつスライド
-//         slidesToShow: 3,
-//         slidesToScroll: 3,
-//         // スライドの最後まで来たら最初に戻る
-//         infinite: true,
-//         dots: true
-//       }
-//     },
-//     {
-//       breakpoint: 600,
-//       settings: {
-//         // 600px以下の時、スライドは2枚表示、2枚ずつスライド
-//         slidesToShow: 2,
-//         slidesToScroll: 2
-//       }
-//     }
-//   ]
-// });
-
 $('.p-step__sliderList').slick({
   dots: true,
   infinite: true,
@@ -37,3 +5,27 @@ $('.p-step__sliderList').slick({
   fade: true,
   cssEase: 'linear'
 });
+
+// //初回のみモーダルをすぐ出す判定。flagがモーダル表示のstart_open後に代入される
+// var access = $.cookie('access')
+// if(!access){
+//   flag = true;
+//   $.cookie('access', false);
+// }else{
+//   flag = false  
+// }
+
+// //モーダル表示
+// $(".video-open").modaal({
+// start_open:flag, // ページロード時に表示するか
+// overlay_close:true,//モーダル背景クリック時に閉じるか
+// type: 'video',
+// background: '#28BFE7', // 背景色
+// overlay_opacity:0.8, // 透過具合
+// before_open:function(){// モーダルが開く前に行う動作
+//   $('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+// },
+// after_close:function(){// モーダルが閉じた後に行う動作
+//   $('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+// }
+// });
